c2da42d2e08768118e83bc941bc06ad25254ec2c
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-10-21 20:45:58 +0200

    merge master witch dupa (books_client directory)

--- rest_server/rest_server/settings.py
+++ rest_server/rest_server/settings.py
@@ -124,8 +124,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
