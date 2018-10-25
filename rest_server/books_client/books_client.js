532dd6aaf7b1d47a6fbe318884b522631a38170d
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-10-25 22:13:56 +0200

    adding new book and displaying details of existent

--- rest_server/books_client/books_client.js
+++ rest_server/books_client/books_client.js
@@ -1,26 +1,70 @@
 $(document).ready(function() {
+
+    let body = $('body');
+    let addingForm = $('#add-form');
+
     $.ajax({
         url: 'http://127.0.0.1:8000/book/',
         data: '',
         type: 'GET',
         dataType: 'json',
     }).done(function(result) {
-        console.log(result);
-        let body = $('body');
         for (let elem of result) {
-            body.append(`<p><div>${elem.id}) ${elem.title}`, {id: elem.id, title: elem.title}, '<div>');
-            let ps = $('p');
-            // ps.text(`${this}) ${}`);
-            // .on('click', unfoldDiv);
+            let p = $('<p>', {id: elem.id, class: "main-p", title: elem.title});
+            let div = $('<div>', {class: "main-div"});
+
+            p.text(`${elem.id}) ${elem.title}`);
+            p.append(div);
+            body.append(p);
+            p.on('click', unfoldDiv)
         }
     }).fail(function(xhr, status, err) {
 
-    }).always(function(xhr, status) {
+    });
 
-    })
+    addingForm.on('submit', addBook);
+
+
+    function unfoldDiv() {
+        let mainDiv = $(this).children('div');
+        let book_id = $(this).attr('id');
+
+        mainDiv.toggle();
+
+        if (mainDiv.children().length === 0) {
+            $.ajax({
+            url: `http://127.0.0.1:8000/book/${$(this).attr('id')}`,
+            data: '',
+            type: 'GET',
+            dataType: 'json'
+            }).done(function(result) {
+                for (let key in result) {
+                    let div = $(`<div>${key}: ${result[key]}</div>`);
+                    mainDiv.append(div);
+                }
+            });
+        }
+
+    }
+
+
+    function addBook(event){
+        event.preventDefault();
+
+        $.ajax({
+            url: 'http://127.0.0.1:8000/book/',
+            data: $(this).serialize(),
+            type: 'POST',
+            dataType: 'json'
+        }).done(function(response){
+            let message = $('#message');
+            message.text('Book has been successfully added to database.');
+            setTimeout(function() {
+                message.fadeOut(4000);
+            }, 500);
+        }).fail(function(xhr, status, err){
+            alert('At least one of input data is invalid.');
+        });
+    }
+
 });
-
-
-function unfoldDiv() {
-    this.next().toggle();
-}
